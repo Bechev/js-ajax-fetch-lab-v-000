64865a2fcd00e60b50df94d9503ec542b2fcb03f
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-08-25 18:24:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,8 +12,14 @@ function showResults(json) {
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
-  //use fetch to fork it!
-}
+  fetch('https://api.github.com/repos/:owner/:repo/forks',{
+    method: 'POST',
+    body: JSON.stringify(postData),
+    headers: {
+      Authorization: `token ${token}`
+    }
+  }).then(res => console.log(res));
+})
 
 function getToken() {
   //change to your token to run in browser, but set
